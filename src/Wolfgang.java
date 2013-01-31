3fa950bf41839264a747f99066b95d7c3e86ef93
it1680505 <it1680505@ou134-206.ad.ilstu.edu>
2013-01-31 14:48:29 -0600

    First commit

---
+++ src/Wolfgang.java
@@ -0,0 +1,42 @@
+/*
+ * Modified on <1/22/2013>
+ * 
+ * ULID: <Rkhickm>
+ * Course: IT 168
+ * Lecture section: <4>
+ * Instructor: <Professor Matsuda>
+ * Lab section: <5>
+ * Instructor <Professor Erickson>
+ */
+
+/**
+ * Demonstrates that I performed the pre-lab correctly as well as demonstrating my skills in interpreting
+ * pre-written machine code into english text
+ * 
+ * @author <Ryan Hickman>
+ *
+ */
+
+// My class; which is public
+public class Wolfgang {
+	// My main method
+	public static void main (String [] args){
+		
+		// This prints out the statement "The works of Wolfgang" on one line and then creates a new line for
+		// "include" to be printed on. "Include" has a space after it.
+		System.out.print("The works of Wolfgang\ninclude ");
+		// From the line that include is on the following is added: "the following"
+		System.out.print("the following");
+		// This statement creates a new line first then prints out: "The Turkish March" with a space after "March"
+		System.out.print("\nThe Turkish March ");
+		// after the space in "March" this statement prints out "and Symphony No. 40" with a space after "40"
+		System.out.print("and Symphony No. 40 ");
+		// This statement prints out "in G minor." after the space in "40"
+		System.out.println("in G minor.");
+		
+		// I successfully interpreted the exercise on my pre-lab assignment. 
+		
+		
+	}
+
+}
